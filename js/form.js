08db45741b2dcbc230279f531e7a52b27d031f26
Khanh Tran <nerkar.tran@gmail.com>
2024-07-07 19:11:38 +0700

    DONE FORM

--- js/form.js
+++ js/form.js
@@ -1,18 +1,19 @@
 const submitBtn = document.querySelector('#mailBtn');
 
-submitBtn.addEventListener('submit', () => {
-    const urlObject = new URL("https://script.google.com/macros/s/AKfycbyqs1upJ111FDObhzGsvhxz0oxNQSAIfbN_e0zsWNebsYTSg79RKAc5HBhFyE5GR7s49A/exec");
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const urlObject = new URL("https://script.google.com/macros/s/AKfycbyPvws7EcmA-GDPEb_yaWRvq2VW5lBexnbTCxhkShYqlV-Ya5LwpVAaBI7N7g13iciY-Q/exec");
 
-    const name = document.querySelector('.mailname').value;
-    const mail = document.querySelector('.mailmail').value;
-    const phone = document.querySelector('.mailphone').value;
-    const message = document.querySelector('.mailmess').value;
+    const name_ = document.querySelector('.mailname').value;
+    const mail_ = document.querySelector('.mailmail').value;
+    const phone_ = document.querySelector('.mailphone').value;
+    const message_ = document.querySelector('.mailmess').value;
 
-    urlObject.searchParams.set("Tên khách hàng", name);
-    urlObject.searchParams.set("Email", mail);
-    urlObject.searchParams.set("Số điện thoại", phone);
-    urlObject.searchParams.set("Lời nhắn", message);
-    urlObject.searchParams.set("Ghi chú", "Liên hệ từ website");
+    urlObject.searchParams.set("name", name_);
+    urlObject.searchParams.set("mail", mail_);
+    urlObject.searchParams.set("phone", phone_);
+    urlObject.searchParams.set("message", message_);
+    urlObject.searchParams.set("note", "Website");
 
     fetch(urlObject.toString());
 });
